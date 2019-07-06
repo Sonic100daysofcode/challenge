a09e0665ab406a7d82580790ebc3cb710a663a77
Sonic100daysofcode <gairagiragira@gmail.com>
2019-07-07 02:43:42 +0900

    [Delete]firebase

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <tab-menu></tab-menu>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+import TabMenu from '@/components/TabMenu'
+
+export default {
+  name: 'App',
+  components: {
+    TabMenu
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TabMenu.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <ol>
+      <li v-for="(item, index) in items" :key=index>
+         <router-link v-bind:to=item.path>
+            {{ item.title }}
+         </router-link>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabMenu',
+  data () {
+    return {
+      items: [
+        { title: 'top', path: '/' },
+        { title: 'profile', path: '/profile' },
+        { title: 'skills', path: '/skills' },
+        { title: 'outputs', path: '/outputs' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+ol {
+  margin-left: auto;
+  width: 480px;
+  display:flex;
+  justify-content: center;
+  padding: 0;
+  list-style: none;
+}
+
+li {
+  width: 100px;
+  height: 50px;
+  margin-left:10px;
+  margin-right:10px;
+  font-size: 14px;
+  background-color: skyblue;
+  border-radius: 8px;
+  position: relative;
+}
+
+li:hover {
+  opacity: 0.4;
+}
+
+/*  router-linkがaタグとして表示されてたのでスタイルをaタグに書いた*/
+a {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  color: white;
+  text-decoration: none;
+  line-height: 50px;
+}
+
+</style>

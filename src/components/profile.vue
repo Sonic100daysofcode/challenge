7a6851e6cb3900f00d78cb81ce0f7178ef699bde
Sonic100daysofcode <gairagiragira@gmail.com>
2019-07-09 09:18:56 +0900

    [Add]Profile items

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -1,12 +1,18 @@
 <template>
-
   <div class="profile">
     <div class="content">
     <h3> {{ name }} </h3>
     <p v-for="t1 in profi">
           <span class="p1">{{ t1.title }}</span>
-          <span class="p2">{{ t1.pro }}</span><br>
+          <span class="p2">{{ t1.pro }}</span>
+    </p></div>
+    <div class="content">
+    <h3> {{ name2 }}</h3>
+    <p v-for=" t2 in proc">
+      <span class="p1">{{t2.title}}</span>
+      <span class="p2">{{t2.pro}}</span>
     </p>
+  </div>
     </div>
   </div>
 </template>
@@ -16,13 +22,20 @@ export default {
   name: 'profile',
   data () {
     return {
-      name: '基本情報',
+      name: '<基本情報>',
       profi:[
        {title:'ニックネーム:',pro:'そにまる'},
        {title:'年齢:',pro:'21 years old'},
        {title:'血液型:',pro:'A型'},
        {title:'出身地:',pro:'兵庫県'}
-]
+      ],
+      name2:'<性格・趣味>',
+      proc:[
+      {title:'性格:',pro:'マイペース'},
+      {title:'趣味:',pro:'キャンプ/モノポリー/HTML'},
+      {title:'タイプ:',pro:'インドア'},
+      {title:'趣味にしたい:',pro:'読書/料理'}
+      ]
     }
   }
 }
@@ -36,7 +49,7 @@ div.profile {
 }
 
 div.content {
-  border-radius: 25px;
+  border-radius: 15px;
   background: #e3f2fd;
   margin: 0px 370px 0px 370px;
 }
@@ -59,7 +72,7 @@ p{
 
 span{
   display:inline-block;
-  width:10em;
+  width:15em;
 }
 
 </style>

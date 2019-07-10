4659f62a1b61050b167b3b1be491a390a7f491f5
Sonic100daysofcode <gairagiragira@gmail.com>
2019-07-10 23:28:22 +0900

    [Add]items

--- src/components/Output.vue
+++ src/components/Output.vue
@@ -1,9 +1,19 @@
 <template>
 
      <div class="output">
-    <div class="content" v-for="i in 2" :key=i>
-    <h3> {{ name }} </h3>
-    <p> {{ text }} </p></div>
+      <br>
+    <div class="content" v-for="t1 in out">
+    <h3> {{ t1.title }} </h3>
+    <p> {{ t1.text }} </p>
+    <p> {{ t1.prog}}</p>
+    <div v-if="t1.title==='でじふぁーむ。'">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/OFlpBERf_1c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+    <div v-if="t1.title==='チャリ共'"><iframe width="560" height="315" src="https://www.youtube.com/embed/L9KfMYbF5kw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  </div>
+
+
   </div>
 </template>
 
@@ -12,9 +22,12 @@ export default {
   name: 'Output',
   data () {
 
-    return {
-      name: 'お腹すいた',
-      text: 'ご飯を食べたいです。',
+    return{
+      name: '作成物',
+      out:[
+      {title:'でじふぁーむ。',text: 'これは学生の提出物を管理するWebベースのアプリケーションです。',prog:'HTML CSS Javascript Python3 postgre'},
+      {title:'チャリ共',text:'これは自転車を使っているユーザ向けのマップアプリです。',prog:'HTML CSS Javascript PHP MySQL'}
+    ]
     }
   }
 }
@@ -29,9 +42,8 @@ div.output {
 
 div.content {
   background-color: #;
-  display: inline-block;
   margin: 10px;
-  width: 30%;
+  width: 100%;
 }
 
 h3 {
@@ -39,9 +51,8 @@ h3 {
 }
 
 p {
-  width: 90%;
-  display: inline-block;
-  text-align: left;
+  width: 100%;
+  text-align: center;
 }
 
 </style>
